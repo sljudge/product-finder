9fabc2d19b0fa0a3f217da489f0f535f1430bafb
Sam Judge <samuel.judge.92@gmail.com>
2022-10-22 10:10:03 +0100

    Footer component

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Footer from './Footer';
+
+describe('Footer', () => {
+   it('renders without error', () => {
+
+   });
+});
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Logo from "assets/images/logo.svg";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="px-page py-12 text-white text-sm bg-blue-300">
+      <img src={Logo} alt="Huguenots logo" />
+      <p className="my-8 max-w-3xl">
+        Nostrud dolor consectetur eiusmod laboris do irure minim et mollit anim
+        est ex. Ut ullamco enim ipsum voluptate et dolore voluptate nisi enim in
+        aute in ex dolore. Laborum non cupidatat est nisi tempor veniam
+        reprehenderit irure eu eiusmod. Veniam culpa excepteur sunt veniam
+        nostrud do deserunt proident deserunt quis aliqua occaecat nulla
+        aliquip.
+      </p>
+      <div>
+        <a href="#" target="_blank" className="mr-4">
+          Terms of Use
+        </a>
+        <a href="#" target="_blank" className="mr-4">
+          Legal Terms
+        </a>
+        <a href="#" target="_blank" className="mr-4">
+          Privacy Policy
+        </a>
+        <a href="#" target="_blank" className="mr-4">
+          Cookie Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
