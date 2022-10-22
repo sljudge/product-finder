cde8cd90cd1289470cce26b862ee18f107757e9f
Sam Judge <samuel.judge.92@gmail.com>
2022-10-22 10:14:57 +0100

    render page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
 
-import { store } from "./config/store";
+import { store } from "config/store";
+import ProductTable from "pages/ProductTable";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <></>
+      <ProductTable />
     </Provider>
   );
 };
